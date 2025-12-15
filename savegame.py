b0f4a112ec5cfa3d48864e38dd47796b458b2c7b
Victor BON <lionvb.616@gmail.com>
2025-12-16 00:20:15 +0100

    Ajout ded class pour la sauvegarde de partie

--- savegame.py
+++ savegame.py
@@ -1,13 +1,13 @@
 from pymongo import*
-url = "mongodb+srv://botdiscord:botdiscord2025@databasejeu.sklp0ra.mongodb.net/?appName=databasejeu"
-client=MongoClient(url)
-db=client["Jeu_de_dames"]
-Games=db["Games"]
-i=count = Games.count_documents({})
-class Partie:
-    def __init__(self,i:int):
-        self.nb=i
-        self.name=f"Partie {i+1}"
+
+class Partie_database:
+    def __init__(self):
+        self.url = "mongodb+srv://botdiscord:botdiscord2025@databasejeu.sklp0ra.mongodb.net/?appName=databasejeu"
+        self.client=MongoClient(self.url)
+        self.db=self.client["Jeu_de_dames"]
+        self.Games=self.db["Games"]
+        self.nb= self.Games.count_documents({})
+        self.name=f"Partie {self.nb+1}"
         self.coup=[]
         self.nb_coup=0
         self.winner=None
@@ -16,13 +16,18 @@ class Partie:
     def coups(self,coup_joue:str):
         self.coup.append(coup_joue)
         self.nb_coup+=1
-    def dic(self):
+    def dict(self):
         self.dic={"Name":self.name,"Winner":self.winner,"Nb coup":self.nb_coup}
-        for i in range(len(self.coup)):
-                self.dic[f"Coup {i+1}"]=self.coup[i]
-        return self.dic
-Parti=Partie(i)
-Parti.coups("G:B8")
-#Games.delete_one({"Name":"Partie "})
-#Games.insert_one(Parti.dic()) #test 
-print(Parti.dic())
+        for j in range(len(self.coup)):
+                self.dic[f"Coup {j+1}"]=f"{self.coup[j]}  /  {j+1}"
+    def add_data(self):
+        print(self.dic)
+        self.Games.insert_one(self.dic)
+
+Partie=Partie_database()
+Partie.coups("G:B8")
+
+#dict=Partie.dic()
+#Partie.Games.insert_one(dict) #test 
+#Partie.Games.delete_one({"Name":"Partie 1"})
+#print(dict)
